769b9fa873544936d1ead58a46a0a46d6229b466
Kenzachoukry <91221345+Kenzachoukry@users.noreply.github.com>
2023-08-06 23:35:09 -0400

    Revert "Update cipher.py"

--- cipher.py
+++ cipher.py
@@ -1,13 +1,1 @@
-def encrypt(text, shift=5):
-    empty_text = ""
-    for char in text:
-        if char.isalpha():
-            is_upper_case = char.isupper()
-            char = char.lower()
-            encrypted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
-            if is_upper_case:
-                encrypted_char = encrypted_char.upper()
-        else:
-            encrypted_char = char
-        empty_text += encrypted_char
-    return encrypted_text
+# add your code here
