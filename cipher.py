595f4aef4b919ff6cc985d2bb8d72861c2d8c35e
Kenzachoukry <91221345+Kenzachoukry@users.noreply.github.com>
2023-08-06 23:28:07 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,13 @@
-# add your code here
+def encrypt(text, shift=5):
+    empty_text = ""
+    for character in text:
+        if character.isalpha():
+            is_upper_case = character.isupper()
+            character = character.lower()
+            encrypted_character = chr((ord(character) - ord('a') + shift) % 26 + ord('a'))
+            if is_upper_case:
+                encrypted_character = encrypted_character.upper()
+        else:
+            encrypted_character = character
+        empty_text += encrypted_character
+    return empty_text
